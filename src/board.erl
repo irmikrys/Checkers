5f95b2dd62d9ad10322bf574003f581373bba4cc
Irmina Krysiak <irmina.krysiak@gmail.com>
2018-01-26 21:13:52 +0100

    [Logic] Adding draught to board.

--- src/board.erl
+++ src/board.erl
@@ -3,7 +3,7 @@
 %%% Created : 27. Dec 2017 12:13
 %%%-------------------------------------------------------------------
 -module(board).
--export([showBoard/1, emptyBoard/0, getFieldColor/1]).
+-export([showBoard/1, emptyBoard/0, addToBoard/3, getPosFigure/2]).
 
 -include("constants.hrl").
 
@@ -23,6 +23,20 @@ getKeys(Board) ->
 
 %%maps:get(Key, Map, Default) - jakie value ma zwrocic jak nie znajdzie klucza
 
+
+%%--------------------------------------------------------------------
+
+addToBoard(Pos, Draught, Board) ->
+  IsOccupied = isPosOccupied(Board, Pos),
+  Add = if
+          IsOccupied == false -> maps:put(Pos, Draught, Board);
+          true -> throw(cannot_add_to_board)
+        end,
+  Add.
+
+isPosOccupied(Board, Position) ->
+  maps:is_key(Position, Board).
+
 %%------------------------- showing board ----------------------------
 
 showBoard(Board) ->
@@ -33,15 +47,15 @@ showBoard(Board) ->
     "~n".
 
 getAllStringRows(Board) ->
-  lists:concat([packRow(Board, Row) ++ "\n" || Row <- lists:seq(1,8)]).
-
-getStringRow(Board, Row) ->
-  lists:concat([showField(getFieldType(Board, {Row, Col})) ++ " " || Col <- lists:seq(1, 8)]).
+  lists:concat([packRow(Board, Row) ++ "\n" || Row <- lists:seq(1, 8)]).
 
 packRow(Board, Row) ->
   leftSideLabelPack(lists:nth(Row, ?NUMS)) ++
     getStringRow(Board, Row) ++
     rightSideLabelPack(lists:nth(Row, ?NUMS)).
+
+getStringRow(Board, Row) ->
+  lists:concat([showField(getFieldType(Board, {Row, Col})) ++ " " || Col <- lists:seq(1, 8)]).
 
 showField(FieldType) ->
   Field = if
@@ -55,13 +69,8 @@ showField(FieldType) ->
           end,
   Field.
 
-getFieldType(Board, {X, Y}) ->
-  FindResult = maps:is_key({X, Y}, Board),
-  Type = if
-           FindResult == true -> maps:get({X, Y}, Board);
-           true -> {getFieldColor({X, Y}), field}
-         end,
-  Type.
+getFieldType(Board, Position) ->
+  maps:get(Position, Board, {getFieldColor(Position), field}).
 
 getFieldColor({X, Y}) when (X + Y) rem 2 == 1 -> black;
 getFieldColor({X, Y}) when (X + Y) rem 2 == 0 -> white;
